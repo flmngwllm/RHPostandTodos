db3b2ca707112ad7c9457b574c483e582f90bca1
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-07-09 20:31:19 -0400

    added a componentDidUpdate that updates the resources and checks whether to update them or not

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -11,6 +11,15 @@ async componentDidMount(){
     this.setState({resources: response.data})
 }
 
+//Updates the resource again and decides whether or not the previous resource has changed
+async componentDidUpdate(prevProps){
+    if (prevProps.resource !== this.props.resource){
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+    this.setState({resources: response.data})
+    }
+
+}
+
     render() {
         return (
             <div>
