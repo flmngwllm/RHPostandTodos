785a793b04eed8dd694e9a528622d36dd35296cc
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-07-10 13:46:20 -0400

    seperated the react hooks stuff into its own function for reuse

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,23 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
+// function for react hooks useState and useEffect
+const useResources = (resource) => {
+//setting th state to an empty array
+const [resources, setResources] = useState([]);
+
+const fetchResource = async () =>{
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+    setResources(response.data)
+
+}
+
+useEffect(() => {
+    fetchResource(resource)
+    },
+    [resource]
+)
+
+return resources
+}
+
+
+//functional component
 const ResourceList = ({resource}) => {
-
-    //setting th state to an empty array
-    const [resources, setResources] = useState([]);
-
-    const fetchResource = async () =>{
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
-        setResources(response.data)
-
-    }
-
-    useEffect(() => {
-        fetchResource(resource)
-        },
-        [resource]
-    )
-
+    const resources = useResources(resource)
+    
         return (
             <ul>
                 {resources.map(record => (<li key={record.id}>{record.title}</li>))}
